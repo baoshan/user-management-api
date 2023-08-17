a6f85a2c540d9f42389db10ce0b3ef45ac8281f8
Baoshan Sheng <sheng@icmd.org>
2023-08-17 11:39:40 +0800

    fix: missing file for creating administrator

---
+++ src/utils/create_admin_user.ts
@@ -0,0 +1,19 @@
+import { sql } from "./postgres.js";
+import { signJWT } from "./sign_jwt.js";
+import type { AuthenticatedUser, NewUser, UserRow } from "../types.js";
+import bcrypt from "bcrypt";
+import { pass } from "fp-ts/lib/Writer.js";
+
+export async function createAdminUser(
+  newUser: NewUser,
+): Promise<AuthenticatedUser> {
+  const password = await bcrypt.hash(newUser.password, 10);
+  const [user] = await sql`
+    INSERT INTO users (name, email, password, admin)
+    VALUES (${newUser.name}, ${newUser.email}, ${password}, true)
+    RETURNING users.id, users.name, users.email, users.admin
+    ` as [UserRow];
+  const jwt = signJWT(user.id, true);
+  const result: AuthenticatedUser = { ...user, jwt };
+  return result;
+}
